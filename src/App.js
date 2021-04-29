1bea05aa01625f6204bd8baec623e8482daf6801
ericleroy1 <54584831+ericleroy1@users.noreply.github.com>
2021-04-29 19:24:56 +1000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import DataTable from "./components/DataTable";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>COVID Tracker</h1>
       </header>
+      <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example">
+        <Tab eventKey="home" title="Home">
+          {/* <Sonnet /> */}
+        </Tab>
+        <Tab eventKey="profile" title="Vaccine Data">
+          {/* <Sonnet /> */}
+        </Tab>
+        <Tab eventKey="contact" title="Worsening">
+          {/* <Sonnet /> */}
+        </Tab>
+        <Tab eventKey="contact" title="Improving">
+          {/* <Sonnet /> */}
+        </Tab>
+      </Tabs>
+
+        <form>
+          <label>
+            Search country:
+            <input type="text" name="name" />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+
+    <DataTable />
+
     </div>
   );
 }
